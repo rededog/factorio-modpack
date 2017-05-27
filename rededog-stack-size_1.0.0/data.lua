cd0c8cc96beb7d518c995408facbc203b6dd7ce5
Joel Koglin <JoelKoglin@gmail.com>
2017-05-26 19:10:12 -0700

    removing pressure tanks since it wasn't loading correctly adding stack size mod

---
+++ rededog-stack-size_1.0.0/data.lua
@@ -0,0 +1,5 @@
+data.raw.item["sulfur"].stack_size = 200
+data.raw.item["solid-fuel"].stack_size = 200
+data.raw.item["uranium-235"].stack_size = 200
+data.raw.item["uranium-238"].stack_size = 200
+data.raw.item["uranium-ore"].stack_size = 200
